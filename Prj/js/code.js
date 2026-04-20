f3446b63e8f1ad371445fa94ed39060b5b6cd2a1
Quatar <222828688+QtrCmmn@users.noreply.github.com>
2026-04-20 15:40:42 +0300

    Add files via upload

---
+++ Prj/js/code.js
@@ -0,0 +1,19 @@
+//Начальный экран
+//Смена цветов фона
+{
+document.getElementById('change_col_body_dark').addEventListener("click", change_col_dark)
+document.getElementById('change_col_body_gray').addEventListener("click", change_col_gray)
+document.getElementById('change_col_body_white').addEventListener("click", change_col_white)
+function change_col_dark() {
+    var main = document.getElementById('body');
+    main.style.backgroundColor= "#292b29";
+}
+function change_col_gray() {
+    var main = document.getElementById('body');
+    main.style.backgroundColor= "#626770";
+}
+function change_col_white() {
+    var main = document.getElementById('body');
+    main.style.backgroundColor= "#d6d9dd";
+}
+}
